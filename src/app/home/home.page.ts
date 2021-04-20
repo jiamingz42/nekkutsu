cfe6061a617a0cfcbd367790d17e3c23375e8757
Jiaming Zhang <jason721b@gmail.com>
2021-04-20 15:09:48 -0700

    Add mp3/vtt files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,6 +5,7 @@ import { IonRange } from '@ionic/angular';
 export interface Track {
   name: string;
   path: string;
+  subtitle: string;
 }
 
 @Component({
@@ -17,10 +18,42 @@ export class HomePage {
     {
       name: 'S01E01',
       path: './assets/mp3/S01E01.mp3',
+      subtitle: './assets/mp3/S01E01.vtt',
+    },
+    {
+      name: 'S01E02',
+      path: './assets/mp3/S01E02.mp3',
+      subtitle: './assets/mp3/S01E02.vtt',
+    },
+    {
+      name: 'S01E03',
+      path: './assets/mp3/S01E03.mp3',
+      subtitle: './assets/mp3/S01E03.vtt',
+    },
+    {
+      name: 'S01E04',
+      path: './assets/mp3/S01E04.mp3',
+      subtitle: './assets/mp3/S01E04.vtt',
+    },
+    {
+      name: 'S01E05',
+      path: './assets/mp3/S01E05.mp3',
+      subtitle: './assets/mp3/S01E05.vtt',
+    },
+    {
+      name: 'S01E06',
+      path: './assets/mp3/S01E06.mp3',
+      subtitle: './assets/mp3/S01E06.vtt',
+    },
+    {
+      name: 'S01E07',
+      path: './assets/mp3/S01E07.mp3',
+      subtitle: './assets/mp3/S01E07.vtt',
     },
     {
       name: 'S01E08',
       path: './assets/mp3/S01E08.mp3',
+      subtitle: './assets/mp3/S01E08.vtt',
     },
   ];
 
