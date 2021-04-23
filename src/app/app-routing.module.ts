70109686729571533e9a50529c1a135a98742014
Jiaming Zhang <jason721b@gmail.com>
2021-04-22 22:35:36 -0700

    Add Component track

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { TrackComponent } from './track/track.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'track/:id',
+    component: TrackComponent
+  }
 ];
 
 @NgModule({
